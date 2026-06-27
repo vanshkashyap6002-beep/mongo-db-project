4120d86b44286ed22b4ba999e7c4ded1b57a6af3
vansh kashyap <vanshkashyap6002@gmail.com>
2026-06-27 21:43:03 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL:"https://mongo-db-project.onrender.com"
-
+  baseURL: "https://mongo-db-project.onrender.com/api/tasks",
 });
 
 export default API;
